2f5d8fcc10cd3870daed6594cf3fee5b0c3bf585
zhdanov <p.zhdanov1993@gmail.com>
2018-08-24 21:03:27 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-toolbar
+      app
+      color="teal lighten-3"
+      dark
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>
+          <v-icon>bug_report</v-icon>
+          Link One
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+
+      <router-view>
+
+        </router-view>
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+};
+</script>
