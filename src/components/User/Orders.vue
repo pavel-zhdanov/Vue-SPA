aba9479741b1b3e113e9ca2c504f145833b233f0
zhdanov <p.zhdanov1993@gmail.com>
2018-08-25 11:58:50 +0300

    add orders page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,39 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3 ml-3">Orders</h1>
+        <v-list
+          subheader
+          two-line
+        ><v-list-tile
+          avatar
+          v-for="item in orders"
+          :key="item.id"
+        >
+          <v-list-tile-action>
+            <v-checkbox
+              :input-value="item.done"
+              @click="markDone(item)"
+              color="success"
+              :disabled="item.done"
+            ></v-checkbox>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{item.name}}</v-list-tile-title>
+            <v-list-tile-sub-title>{{item.phone}}</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn
+              color="grey darken-2"
+              class="white--text"
+              :to="'/ad/' + item.adId"
+            >Open</v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +43,16 @@
   export default {
     data() {
       return {
+        orders: [
+          {id: 'sdfsd', name: `Pavel`, phone: '8-900-676-55-55',
+            adId: '1', done: false},
+        ],
       };
+    },
+    methods: {
+      markDone(order) {
+        order.done = true;
+      },
     },
   };
 </script>
